5542e0cfc7c7016bdd1eabf27eafd40977bbb5ff
Gustavout22 <gustavougartetorres@gmail.com>
2023-11-17 00:26:05 -0500

    using React.FC and interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { Todos } from "./components/Todos"
+import { useState } from "react"
+
+const mockTodos = [
+  {
+    id: 1,
+    title: "todo 1",
+    completed: false
+  },
+  {
+    id: 2,
+    title: "todo 2",
+    completed: false
+  },
+  {
+    id: 3,
+    title: "todo 3",
+    completed: false
+  },
+]
+
+
+
 const App = (): JSX.Element => {
+const [todos] = useState(mockTodos)
+
   return (
     <div>
       <h2>Todo mvc</h2>
+      <Todos todos={todos}/>
     </div>
   )
 }
